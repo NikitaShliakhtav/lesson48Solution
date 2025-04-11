7a31917758b7f09c1ecce9e0290b2e22e09b8c62
shakal2004 <bl4ckr0s3@mail.ru>
2025-04-11 18:31:35 +0300

    Add project files.

---
+++ tusk1/logic.cpp
@@ -0,0 +1,49 @@
+#include "logic.h"
+
+int absolut(int number) {
+	return number < 0 ? -number : number;
+}
+
+int get_max_last_index(int* array, int size) {
+	int index = size - 1;
+	for (int i = size - 2; i >= 0; i--)
+	{
+		if (absolut( * (array + i)) > absolut ( * (array + index)))
+			index = i;
+	}
+	return index;
+}
+
+int get_min_last_index(int* array, int size) {
+	int index = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (absolut ( * (array + i)) < absolut ( * (array + index)))
+			index = i;
+	}
+	return index;
+}
+
+
+
+
+int sum_elements_between_extrems(int* array, int size) {
+	int imin = get_min_last_index(array, size);
+	int imax = get_max_last_index(array, size);
+
+	int sum = 0;
+
+	if (imin > imax) {
+		int t = imin;
+		imin = imax;
+		imax = t;
+	}
+
+	for (int i = imin +1; i < imax; i++)
+	{
+		sum += *(array + i);
+	}
+
+	return sum;
+}
---
+++ tusk1/logic.h
@@ -0,0 +1,2 @@
+#include "util.h"
+int sum_elements_between_extrems(int* array, int size);
